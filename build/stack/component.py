4a5780f8099a901c1112cced0de86bdc1d805f8b
Igor Malinovskiy <igor.malinovskiy@redis.com>
2023-05-08 16:52:11 +0200

    Update widget UI - Add links to the widget footer - Fix tab ids - Hide footer for single lang examples - Fix radio buttons on Safari

--- build/stack/component.py
+++ build/stack/component.py
@@ -505,6 +505,9 @@ class Client(Component):
                 example_metadata['highlight'] = e.highlight
                 example_metadata['hidden'] = e.hidden
                 example_metadata['named_steps'] = e.named_steps
+                example_metadata['sourceUrl'] = (
+                    f'{ex["git_uri"]}/tree/{ex["dev_branch"]}/{ex["path"]}/{os.path.basename(f)}'
+                )
                 examples = self._root._examples
                 if example_id not in examples:
                     examples[example_id] = {}
